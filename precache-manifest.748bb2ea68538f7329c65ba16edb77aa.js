d233dc98dd2268171914d1209a03af5e6f194a41
yash handa <yashhanda500@gmail.com>
2020-12-30 16:46:04 +0530

    Updates

---
+++ precache-manifest.748bb2ea68538f7329c65ba16edb77aa.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b66300d35fd567be09ced9bdec97ca89",
+    "url": "/walkman/index.html"
+  },
+  {
+    "revision": "b04cb7b06fdeb6b85d80",
+    "url": "/walkman/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "61cacdf963fe03bf7bf0",
+    "url": "/walkman/static/css/main.25bd5a3a.chunk.css"
+  },
+  {
+    "revision": "b04cb7b06fdeb6b85d80",
+    "url": "/walkman/static/js/2.f9ab0b45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/walkman/static/js/2.f9ab0b45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61cacdf963fe03bf7bf0",
+    "url": "/walkman/static/js/main.895dbeb5.chunk.js"
+  },
+  {
+    "revision": "d6ec059acfdbb21dc506",
+    "url": "/walkman/static/js/runtime-main.36f56703.js"
+  },
+  {
+    "revision": "1b519cda46b9cafabd7a132d2d44e82d",
+    "url": "/walkman/static/media/facebook-logo.1b519cda.svg"
+  },
+  {
+    "revision": "c6b14713fe715f4ba11f27c744bddbe8",
+    "url": "/walkman/static/media/google-logo.c6b14713.svg"
+  },
+  {
+    "revision": "db33e70bc9dee9fa9ae9737ad83d77ba",
+    "url": "/walkman/static/media/gotham_bold.db33e70b.ttf"
+  },
+  {
+    "revision": "8566e2336952927984495865df90963c",
+    "url": "/walkman/static/media/gotham_light.8566e233.ttf"
+  },
+  {
+    "revision": "77171d8f5b5283f9d47a3434704bf944",
+    "url": "/walkman/static/media/gotham_medium.77171d8f.ttf"
+  },
+  {
+    "revision": "d71c39e98eaae61125316a64a5a76aee",
+    "url": "/walkman/static/media/launcher-window-background@2x.d71c39e9.png"
+  },
+  {
+    "revision": "4dd30c4739d84af9ae2a3af5c9fd7100",
+    "url": "/walkman/static/media/sound-wave.4dd30c47.svg"
+  }
+]);
